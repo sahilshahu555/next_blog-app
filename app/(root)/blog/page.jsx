0801450b6d87baf4a6e1fbbfcc7f2f117fc00709
sahilshahu555 <sahilshahu555@gmail.com>
2024-03-07 19:23:53 +0530

    edited blogs page

--- app/(root)/blog/page.jsx
+++ app/(root)/blog/page.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const BlogsPage = () => {
   return (
-    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-5 gap-y-10 " >
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-5 gap-y-10  " >
      <BlogCard/>
      <BlogCard/>
      <BlogCard/>
